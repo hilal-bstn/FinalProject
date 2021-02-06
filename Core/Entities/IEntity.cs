dd0fd152f896eae6d0d8d3ec7856673d731445e4
hilalbastan <hilalbastan@gmail.com>
2021-02-06 23:59:34 +0300

    Add project files.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace  Core.Entities
+{
+    //IEntity imlement eden class veritabanı tablosudur.
+    public interface IEntity
+    {
+
+    }
+}
